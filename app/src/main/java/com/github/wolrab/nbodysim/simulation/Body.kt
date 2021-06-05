dc058c9bae5a0ed4514df6553708ecf2da980514
Connor Barlow <barlowc2@wwu.edu>
2021-06-05 15:39:32 -0700

    Starting to look like something

--- app/src/main/java/com/github/wolrab/nbodysim/simulation/Body.kt
+++ app/src/main/java/com/github/wolrab/nbodysim/simulation/Body.kt
@@ -1,8 +1,7 @@
 package com.github.wolrab.nbodysim.simulation
 
-class Body(val mass: Double, var position: Vector, var velocity: Vector = Vector(arrayOf(0f, 0f, 0f)), var acceleration: Vector = Vector(arrayOf(0f, 0f, 0f))) {
-
-    fun distTo(body: Body) {
-        val distSquared = body.position.x
+class Body(val mass: Double, var position: Vector, var velocity: Vector = Vector(arrayOf(0.0, 0.0, 0.0)), var acceleration: Vector = Vector(arrayOf(0.0, 0.0, 0.0))) {
+    fun distTo(body: Body): Double {
+        return (body.position - position).magnitude
     }
 }
--- app/src/main/java/com/github/wolrab/nbodysim/simulation/PhysicsManager.kt
+++ app/src/main/java/com/github/wolrab/nbodysim/simulation/PhysicsManager.kt
@@ -1,7 +1,7 @@
 package com.github.wolrab.nbodysim.simulation
 
-class PhysicsManager(dt: Double = 0.1f) {
-    fun update() {
+class PhysicsManager(dt: Double = 0.05) {
+    fun update(bodies: List<Body>) {
 
     }
 }
